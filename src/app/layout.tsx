6bc7b17e1e4d94cfa67e2bd4de71c73f89f27569
Zamderax <zamderax@gmail.com>
2025-08-08 15:03:09 -0700

    feat: import Metadata type from 'next' in layout.tsx for improved type safety

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 
-import type { Metadata } from 'next';
 import { Analytics } from '@vercel/analytics/react';
+import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import Footer from '@/components/layout/footer';
